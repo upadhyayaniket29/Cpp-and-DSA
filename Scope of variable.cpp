e00252c73becf8d7c817557582daeb5ab9029f9e
Aniket Upadhyay <88603347+upadhyayaniket29@users.noreply.github.com>
2024-03-28 12:07:43 +0530

    Add files via upload

---
+++ Scope of variable.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    // int n=10;
+    // int i=30;
+    // {
+    //   char n='a';
+    //   cout<<n<<endl;
+    // }
+    // cout<<n; 
+    int i=50;
+    for(;i<=100;i++)
+    {
+        cout<<i<<endl;
+    }
+    cout<<i;
+    return 0;
+}
