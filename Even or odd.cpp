4797d657089509ef975b2bcf60a6364f05f1aff5
Aniket Upadhyay <88603347+upadhyayaniket29@users.noreply.github.com>
2024-03-28 12:17:11 +0530

    Add files via upload

---
+++ Even or odd.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int a;
+a==22;
+if (a%2==0)
+cout<<"even";
+else cout<<"odd";
+
+    return 0;
+}
