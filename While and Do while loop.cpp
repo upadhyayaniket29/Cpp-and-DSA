c313b218097dc266542528d467b3e5a3542e7328
Aniket Upadhyay <88603347+upadhyayaniket29@users.noreply.github.com>
2024-03-28 12:09:44 +0530

    Add files via upload

---
+++ While and Do while loop.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     int i=1;
+//     int sum;
+//     sum=0;
+//     int n;
+//     cout<<"enter the value of n";
+//     cin>>n;
+    
+//     do{
+        
+        
+        
+//          i++;
+//         sum=sum+i;
+//         cout<<sum;
+        
+       
+//     }
+//     while(i<=n);
+    
+    
+// }
+//  Continue 
+for(int i=1;i<100;i++){
+    
+    if(i%4==0)
+        
+    
+    continue;
+    
+    
+cout<<i<<" ";
+
+}
+
+}
+
+
+
+
