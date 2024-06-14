b810dd1d34554060ced528535866f6eaa1d6aeb7
Aniket Upadhyay <88603347+upadhyayaniket29@users.noreply.github.com>
2024-06-14 06:16:44 +0000

    Hw Day-05

---
+++ Day-05 HW.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int i,n,sum;
+   sum=0;
+   cout<<"enter the value of n";
+   cin>>n;
+   for(i=0;i<=n;i++){
+    sum+=i;
+    cout<<sum<<endl;
+
+    
+   }
+
+   
+    return 0;
+}
